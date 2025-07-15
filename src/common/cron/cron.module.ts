e83e0c4970fec1ad2284376571b8dc8119af77c2
gab-cat <catimbanggabriel@gmail.com>
2025-07-15 10:39:20 +0800

    Implement project application notification system with cron jobs and email template

--- src/common/cron/cron.module.ts
+++ src/common/cron/cron.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { CronService } from './cron.service';
 import { UsersModule } from '../../users/users.module';
 import { LoggerModule } from '../logger/logger.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { EmailModule } from '../email/email.module';
 
 @Module({
-  imports: [UsersModule, LoggerModule],
+  imports: [UsersModule, LoggerModule, PrismaModule, EmailModule, CqrsModule],
   providers: [CronService],
   exports: [CronService],
 })
