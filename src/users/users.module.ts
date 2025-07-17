30c60c8e215ddc9ae5056f34d66a26445362c304
gab-cat <catimbanggabriel@gmail.com>
2025-07-17 17:53:10 +0800

    feat(users): integrate CQRS for email handling in UsersService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { EmailModule } from '../common/email/email.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [CqrsModule, EmailModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
